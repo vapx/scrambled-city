ea82dd0d31c9011f08b568165b7ec1579d3a8e1f
edmon2 <g.edmon25@gmail.com>
2023-06-14 22:35:15 +0800

    refactor: change design of login page

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -8,12 +8,13 @@ import {
 } from '../../config/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import './Login.css'
+import Logo from '../../assets/googlelogo.svg'
 
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [user, loading] = useAuthState(auth)
-  const [loginSuccess, setLoginSuccess] = useState(false) // New state variable
+  const [loginSuccess, setLoginSuccess] = useState(true)
   const navigate = useNavigate()
 
   useEffect(
@@ -40,35 +41,51 @@ function Login() {
   }
 
   return (
-    <div className="login">
-      {loginSuccess &&
-        <p className="alert alert-success">Successfully Login</p>}
-      <div className="login__container">
-        <input
-          type="text"
-          className="login__textBox"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <input
-          type="password"
-          className="login__textBox"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button className="login__btn" onClick={handleLogin}>
-          Login
-        </button>
-        <button className="login__btn" onClick={logGoogleUser}>
-          Login with Google
-        </button>
-        <div>
-          <Link to="/reset-password">Forgot Password</Link>
-        </div>
-        <div>
-          Don't have an account? <Link to="/registration">Register</Link> now.
+    <div className="w-100 d-flex justify-content-between align-content-center">
+      <div className="left-side-screen-container w-50">
+        <h3>
+          <i>
+            "Test your cyber security knowledge and strengthen your defenses."
+          </i>
+        </h3>
+        <div className="imgIntro" />
+      </div>
+      <div className="login w-50">
+        <div className="login__container">
+          <h3 className="fw-bold">Hello Again!</h3>
+          <p className="lead">You've been missed.</p>
+          <input
+            type="text"
+            className="login__textBox"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="E-mail Address"
+          />
+          <input
+            type="password"
+            className="login__textBox"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button className="login__btn btn btn-primary" onClick={handleLogin}>
+            Login
+          </button>
+          <button
+            className="login__btn-google btn btn-danger w-100 d-flex justify-content-center"
+            onClick={logGoogleUser}
+          >
+            Continue with
+            <img src={Logo} style={{ height: '30px', paddingLeft: '10px' }} />
+          </button>
+          <div>
+            <Link to="/reset-password">Forgot Password</Link>
+          </div>
+          <div>
+            Don't have an account? <Link to="/registration">Register</Link> now.
+          </div>
+          {loginSuccess &&
+            <p className="alert alert-success">Successfully Login</p>}
         </div>
       </div>
     </div>
